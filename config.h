038e5405126fa0d4379f0a686c557291a93c872d
AidanF86 <aidan.t.foley@gmail.com>
2021-02-18 20:55:17 -0500

    idk

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
  */
 static char *font = "Fira Code Medium:pixelsize=26:antialias=true:autohint=true";
 /* Spare fonts */
-static char *font2[] = { "JoyPixels:pixelsize=26:antialias=true:autohint=true" };
+static char *font2[] = { "Twemoji:pixelsize=26:antialias=true:autohint=true" };
 
 
 static int borderpx = 2;
@@ -110,7 +110,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.7;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
